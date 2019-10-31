a53c98159288c1e3b060d636f8caf4fb0423596e
Aleksandra Zuchewicz <aleksandra.zuchewicz@gmail.com>
2019-10-31 15:58:21 +0000

    Array: multiple element by 2 if even, multiple element by 3 if odd - Task

---
+++ Task_13.js
@@ -0,0 +1,37 @@
+"use strict";
+
+/*
+ * Modify and return the array so that all even elements are doubled and all odd elements are tripled.
+ *
+ * Parameter(s):
+ * nums: An array of numbers.
+ */
+let nums = [1, 2, 3, 4, 5];
+function modifyArray(nums) {
+  let newArray = [];
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] % 2 == 0) {
+      let even = nums[i] * 2;
+      newArray.push(even);
+    } else {
+      let odd = nums[i] * 3;
+      newArray.push(odd);
+    }
+  }
+  return newArray;
+}
+console.log(modifyArray(nums));
+
+function main() {
+  const n = +readLine();
+  const a = readLine()
+    .split(" ")
+    .map(Number);
+
+  console.log(
+    modifyArray(a)
+      .toString()
+      .split(",")
+      .join(" ")
+  );
+}
