86027ced3b1280167c807eb9592d0cad7ec50217
Aleksandra Zuchewicz <aleksandra.zuchewicz@gmail.com>
2019-10-31 12:24:28 +0000

    Throw Error Task

---
+++ Task_9.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/*
+ * Complete the isPositive function.
+ * If 'a' is positive, return "YES".
+ * If 'a' is 0, throw an Error with the message "Zero Error"
+ * If 'a' is negative, throw an Error with the message "Negative Error"
+ */
+function isPositive(a) {
+  if (a > 0) {
+    return "YES";
+  } else if (a === 0) {
+    throw Error("Zero Error");
+  } else {
+    throw Error("Negative Error");
+  }
+}
+
+console.log(isPositive(-10));
